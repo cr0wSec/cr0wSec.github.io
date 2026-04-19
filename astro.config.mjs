481b5cb1617eb92c75b49191d3804f7f81e0251e
John Doe <204260440+cr0wSec@users.noreply.github.com>
2026-04-19 18:08:35 +0200

    set site url in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,44 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
-import pagefind from 'astro-pagefind';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypeSlug from 'rehype-slug';
-import { remarkAlert } from 'remark-github-blockquote-alert';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import pagefind from "astro-pagefind";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import { remarkAlert } from "remark-github-blockquote-alert";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), pagefind()],
-	markdown: {
-		shikiConfig: {
-			theme: 'github-dark',
-			wrap: true,
-		},
-		remarkPlugins: [remarkAlert],
-		rehypePlugins: [
-			rehypeSlug,
-			[
-				rehypeAutolinkHeadings,
-				{
-					behavior: 'append',
-					properties: {
-						className: ['heading-anchor'],
-						ariaLabel: 'Link to this section',
-					},
-					content: {
-						type: 'element',
-						tagName: 'span',
-						properties: { className: ['heading-anchor-icon'], ariaHidden: 'true' },
-						children: [{ type: 'text', value: '#' }],
-					},
-				},
-			],
-		],
-	},
+  site: "https://cr0wSec.github.io",
+  integrations: [mdx(), sitemap(), pagefind()],
+  markdown: {
+    shikiConfig: {
+      theme: "github-dark",
+      wrap: true,
+    },
+    remarkPlugins: [remarkAlert],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+          properties: {
+            className: ["heading-anchor"],
+            ariaLabel: "Link to this section",
+          },
+          content: {
+            type: "element",
+            tagName: "span",
+            properties: {
+              className: ["heading-anchor-icon"],
+              ariaHidden: "true",
+            },
+            children: [{ type: "text", value: "#" }],
+          },
+        },
+      ],
+    ],
+  },
 });
